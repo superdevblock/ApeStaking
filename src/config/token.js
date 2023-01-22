70e523a4490bba44bb3264b665ec47434b4f7e34
asinoe <120493933+asinoe@users.noreply.github.com>
2023-01-22 18:58:23 +0900

    upload

---
+++ src/config/token.js
@@ -0,0 +1,15 @@
+export default {
+    staking: {
+        1: "0x3C01c6c58E61Ae5fFc0e603abd7ebe5d9d27D97a",
+        4: "0x3C01c6c58E61Ae5fFc0e603abd7ebe5d9d27D97a",
+    },
+    ape: {
+        1: "0x73Fd64FEED39ba564326Ac4327b90596a2056404",
+        4: "0x73Fd64FEED39ba564326Ac4327b90596a2056404",
+    },
+    variable: {
+        1: "0xAD93D504631feCA691d0a6EFed72f8344Ee72925",
+        4: "0xAD93D504631feCA691d0a6EFed72f8344Ee72925",
+    },
+  };
+  
